ffff0914edeb4469c4ad3fec6ec66c9a104c16b6
ntk-batch12 <ahmad.noor24@outlook.com>
2025-02-02 21:23:25 -0500

    Implemented add() divide() method to handle dropdown for US123

--- src/a/ADemo.java
+++ src/a/ADemo.java
@@ -1,9 +1,17 @@
 package a;
 
 public class ADemo {
-	
+
 	public static void main(String[] args) {
-		System.out.println("ADemo: TeamC");
+		System.out.println("Branch Dev: XYZ");
+	}
+
+	public void add() {
+		System.out.println(1 + 2);
+	}
+
+	public void divide() {
+		System.out.println(2 / 1);
 	}
 
 }
