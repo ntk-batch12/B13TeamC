14d208bbb60fbea6e75588faa4b4c62c09f0af62
ntk-batch12 <ahmad.noor24@outlook.com>
2025-02-07 18:47:57 -0500

    conflict example

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -4,6 +4,9 @@ public class BDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamC");
+		String firstName  = "JOhn";
+		String lastName ="Cena";
+		double price = 33.333;
 	}
 
 }
